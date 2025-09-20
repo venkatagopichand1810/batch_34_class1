507de7dddcd94eef063b365eb38607480cd4776c
venkatguvi <venkatagopichand9@gmail.com>
2025-09-20 10:18:10 +0530

    pushed all the files

---
+++ main.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+console.log( a + b)
+
+//future create the backend using the javascript which will be running outside of the browser
+
+
+//javascript can be run both in browser and outside the browser
+
+//vs code t0 create the FE,,,,in the browser
+
+// vs code to create the BE ....it will not run in the browser...in the terminal
+
+//finally javascript is used to create the FE and BE..MERN STACK
